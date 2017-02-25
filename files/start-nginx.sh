6c8b38c04112f5d661e24945520387bccd34ced9
azcoigreach <azcoigreach@gmail.com>
2017-02-25 00:29:23 -0700

    Update start-nginx.sh

--- files/start-nginx.sh
+++ files/start-nginx.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-if env | grep -q ^NGINX_NO_UPDATE=
-then
-	echo Update of Nginx disabled.
-else
-	echo Updating Nginx
-	apt-get update
-	apt-get upgrade -y nginx
-	apt-get -y clean
-fi
+#if env | grep -q ^NGINX_NO_UPDATE=
+#then
+#	echo Update of Nginx disabled.
+#else
+#	echo Updating Nginx
+#	apt-get update
+#	apt-get upgrade -y nginx
+#	apt-get -y clean
+#fi
 
 
 echo Starting Nginx
